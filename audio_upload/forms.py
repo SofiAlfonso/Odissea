a96ca7a1636a59cc20973890ab0b8e4fea91ffbc
David Alejandro Ramírez Agudelo <131161510+DavidRamirez1106@users.noreply.github.com>
2024-10-16 07:07:46 -0500

    Update forms.py

--- audio_upload/forms.py
+++ audio_upload/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from .models import AudioFile  # Importa tu modelo real
+from .models import AudioFile  
 
 class AudioFileForm(forms.ModelForm):
     language_choices = [
         ('es', 'Español'),
         ('en', 'Inglés'),
         ('fr', 'Francés'),
-        # Agrega más idiomas según sea necesario
+     
     ]
     language = forms.ChoiceField(choices=language_choices, label="Selecciona el idioma")
 
     class Meta:
-        model = AudioFile  # Reemplaza con tu modelo real
-        fields = ['audio', 'language']  # Asegúrate de incluir el campo 'language'
+        model = AudioFile 
+        fields = ['audio', 'language']  
 
